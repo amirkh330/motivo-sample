86637d5bf27343f5f568d7cda5e949fc1498b08a
amirkh330 <amir73khakbaz@gmail.com>
2024-10-02 14:55:33 +0330

    add disable

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 // next.config.js
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,    // Ensures service worker is registered
-    skipWaiting: true,
-  });
-  
-  module.exports = withPWA({
-    // Other Next.js config options can go here
-    reactStrictMode: true,
-    swcMinify: true,
-  });
-  
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true, // Ensures service worker is registered
+  skipWaiting: true,
+  disable: false,
+});
+
+module.exports = withPWA({
+  // Other Next.js config options can go here
+  reactStrictMode: true,
+  swcMinify: true,
+});
