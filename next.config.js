f65c713ecb8da02663bafd544318aad123fdac18
amirkh330 <amir73khakbaz@gmail.com>
2024-10-02 12:42:11 +0330

    change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 // next.config.js
 const withPWA = require('next-pwa')({
     dest: 'public',
+    register: true,    // Ensures service worker is registered
+    skipWaiting: true,
   });
   
   module.exports = withPWA({
