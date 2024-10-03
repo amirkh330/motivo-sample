3e5248e939f09dc87a2bb1eaf133320897c450bd
amirkh330 <amir73khakbaz@gmail.com>
2024-10-03 11:28:51 +0330

    clean code

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,21 +7,6 @@ export const metadata = {
   description: "PWA application with Next 14",
   generator: "Next.js",
   manifest: "/manifest.json",
-  keywords: ["nextjs", "nextjs14", "next14", "pwa", "next-pwa"],
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-  authors: [
-    { name: "Alldo Faiz Ramadhani" },
-    {
-      name: "Alldo Faiz Ramadhani",
-      url: "https://www.linkedin.com/in/alldofaiz/",
-    },
-  ],
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-  icons: [
-    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
-    { rel: "icon", url: "icons/icon-128x128.png" },
-  ],
 };
 
 export default function RootLayout({
